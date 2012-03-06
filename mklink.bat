83971291ed013761a59464e27b9319bcb7f22762
Shintaro Kaneko <kaneshin0120@gmail.com>
2012-03-06 03:38:02 -0800

    Rename ln.sh to syln.sh and mklink.bat to symklink.bat

--- mklink.bat
+++
@@ -1,5 +0,0 @@
-@echo on
-set LINKDIR=%HOMEDRIVE%%HOMEPATH%
-set TARGETDIR=C:\Users\kaneshin\Dropbox\dotfiles
-mklink %LINKDIR%\.vimrc %TARGETDIR%\.vimrc
-mklink %LINKDIR%\.gvimrc %TARGETDIR%\.gvimrc
