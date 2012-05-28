90ef33a1f0455669f2e75ec8bcbcdfef28b8b03e
Shintaro Kaneko <kaneshin0120@gmail.com>
2012-05-27 19:52:29 -0700

    Improve symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
 
 cd $(dirname ${0})
-for dotfile in .?*; do
-    case "$dotfile" in
-        ".." )
-            continue ;;
-        \.git* )
-            continue ;;
-        ".doskey" )
-            continue ;;
-        * )
-            ln -Fis "$PWD/$dotfile" $HOME ;;
-    esac
-done
+SANDBOX=$HOME/Dropbox/dev/sandbox
+TARGET=$HOME
+case $1 in
+    make )
+        cd dotfiles/
+        for dotfile in .?*; do
+            if [ -f $dotfile ]; then
+                ln -sf $PWD/$dotfile $TARGET
+                echo "created $TARGET/$dotfile@ as a symbolic link."
+            elif [ $dotfile = '..' ]; then
+                echo "parent directory"
+            elif [ -d $dotfile ]; then
+                cp -fpr $PWD/$dotfile $TARGET/
+                echo "copy $TARGET/$dotfile"
+            fi
+        done
+    ;;
+    rm )
+        cd $TARGET
+        for dotfile in .?*; do
+            if [ -L $dotfile ]; then
+                rm -f $PWD/$dotfile
+                echo "removed $TARGET/$dotfile@."
+            fi
+        done
+    ;;
+    test )
+        echo "test"
+    ;;
+    * )
+        echo "Require argument"
+        echo "  make"
+        echo "      create symbolic link and copy directory from dotfiles/."
+        echo "  rm"
+        echo "      remove symbolic link in $TARGET."
+    ;;
+esac
 
