c7522f42e30b76defaabf6fdee115464bdc38941
Shintaro Kaneko <kaneshin0120@gmail.com>
2012-06-30 01:50:53 +0900

    Fix c/base-main-bench.c

--- dotfiles/.vim/template/c/base-main-bench.c
+++ dotfiles/.vim/template/c/base-main-bench.c
@@ -3,21 +3,20 @@
 
 int
 main(int argc, char* argv[]) {
-	clock_t t;
 	int i, n;
+	clock_t t;
 	n = 100000000;
 
 	t = clock();
 	for (i = 0; i < n; ++i) {
-        {{_cursor_}}
+		{{_cursor_}}
 	}
 	printf("%ld\n", clock() - t);
 
 	t = clock();
 	for (i = 0; i < n; ++i) {
-		
 	}
-	printf("%d\n", clock() - t);
+	printf("%ld\n", clock() - t);
 
 	return 0;
 }
