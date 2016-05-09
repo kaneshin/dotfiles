7f087499c4483398c59e34fd83da9e67a61a3885
Shintaro Kaneko <kaneshin0120@gmail.com>
2016-05-09 05:18:19 +0000

    Add direnv setting

--- bundle/.zsh/plugins.zsh
+++ bundle/.zsh/plugins.zsh
@@ -1,8 +1,13 @@
 # Maintainer:  Shintaro Kaneko <kaneshin0120@gmail.com>
-# Last Change: 06-May-2016.
+# Last Change: 09-May-2016.
+
+shell=$(echo ${0/*plugins./})
 
 # setup z
 [ -f $LOCALSRC/github.com/rupa/z/z.sh ] && . $LOCALSRC/github.com/rupa/z/z.sh
+
+# setup direnv
+which direnv > /dev/null && eval "$(direnv hook $shell)"
 
 # setup gi
 function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
