015aca12ddc33a3760bf83c559cd5d1ef065838f
Shintaro Kaneko <kaneshin0120@gmail.com>
2016-06-25 11:15:38 +0900

    Add function to grep imported package for golang

--- bundle/.zsh/util.zsh
+++ bundle/.zsh/util.zsh
@@ -1,5 +1,5 @@
 # Maintainer:  Shintaro Kaneko <kaneshin0120@gmail.com>
-# Last Change: 06-May-2016.
+# Last Change: 25-Jun-2016.
 
 function ssh-pubkey() {
   user=$(git config --get github.user)
@@ -8,6 +8,10 @@ function ssh-pubkey() {
     return 1
   fi
   curl -L -s "https://github.com/$user.keys"
+}
+
+function goimp() {
+  grep -e "\t\"\(github\|bitbucket\|golang\|google\).*\"$" . -R --color=none | grep -v -e "\.git" -e "README" -e "wercker" -e "ansible" | cut -f2 | sort -u
 }
 
 # vim:set ts=8 sts=2 sw=2 tw=0:
