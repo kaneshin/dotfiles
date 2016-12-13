30c96b8183e4083b4c487ed768c945e9dd237c82
Shintaro Kaneko <kaneshin0120@gmail.com>
2016-12-13 14:36:55 +0000

    Update

--- bundle/.zsh/keymap.zsh
+++ bundle/.zsh/keymap.zsh
@@ -1,5 +1,5 @@
 # Maintainer:  Shintaro Kaneko <kaneshin0120@gmail.com>
-# Last Change: 04-May-2016.
+# Last Change: 13-Dec-2016.
 
 # make key map
 function _ce() {
@@ -8,16 +8,16 @@ function _ce() {
 }
 zle -N _ce
 
-function _clear_screen() {
-  clear
-  ls -alh | grep --color=none "^d" && ls -la | grep -v "^d\|^total"
-  zle reset-prompt
-  zle -R
-}
-zle -N _clear_screen
+# function _clear_screen() {
+#   clear
+#   ls -alh | grep --color=none "^d" && ls -la | grep -v "^d\|^total"
+#   zle reset-prompt
+#   zle -R
+# }
+# zle -N _clear_screen
 
 bindkey "^O" _ce
-bindkey "^L" _clear_screen
+# bindkey "^L" _clear_screen
 
 # vim:set ts=8 sts=2 sw=2 tw=0:
 # vim:set ft=sh:
